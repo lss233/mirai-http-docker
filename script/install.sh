72d6396417be7bde6e5448b4fe0e8d27ed22b223
Lss233 <i@lss233.com>
2023-02-11 22:48:47 +0800

    Init commit

---
+++ script/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+MCL_VERSION=ce4ab47
+# Exit if any error occured
+set -euxo pipefail
+
+
+wget "https://github.com/iTXTech/mcl-installer/releases/download/${MCL_VERSION}/mcl-installer-${MCL_VERSION#v}-linux-amd64-musl" -O ./mcl-installer
+
+chmod +x ./mcl-installer
+
+mkdir app && cd app
+
+"../mcl-installer" << EOF
+N
+Y
+EOF
+
+chmod +x ./mcl
+
+./mcl --update-package net.mamoe:mirai-api-http --channel stable-v2 --type plugin
+./mcl --update-package xyz.cssxsh.mirai:mirai-device-generator --channel stable-v2 --type plugin
+./mcl --dry-run
+
