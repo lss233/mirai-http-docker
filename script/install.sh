a1ba44e6f3c361c293724135e0cb9fad2e5d7946
Dark Litss <ls1102086624@outlook.com>
2023-03-13 10:22:35 +0800

    fix: use fix-protocol-version 1.3.0

--- script/install.sh
+++ script/install.sh
@@ -12,7 +12,7 @@ chmod +x ./mcl-installer
 mkdir app && cd app
 # 安装临时的 fix-protocol-version 插件
 mkdir plugins
-wget https://github.com/cssxsh/fix-protocol-version/releases/download/v1.2.0/fix-protocol-version-1.2.0.mirai2.jar -O plugins/fix-protocol-version-1.2.0.mirai2.jar
+wget https://github.com/cssxsh/fix-protocol-version/releases/download/v1.3.0/fix-protocol-version-1.3.0.mirai2.jar -O plugins/fix-protocol-version-1.3.0.mirai2.jar
 
 "../mcl-installer" << EOF
 N
