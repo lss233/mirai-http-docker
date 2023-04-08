dc5c99dc3dd4f73773c1bda6ed2ab2b187bdc3b8
Dark Litss <8984680+lss233@users.noreply.github.com>
2023-04-09 01:48:18 +0800

    feat: update to 2.15.0-M1

--- script/install.sh
+++ script/install.sh
@@ -13,6 +13,7 @@ mkdir app && cd app
 # 安装临时的 fix-protocol-version 插件
 mkdir plugins
 wget https://github.com/cssxsh/fix-protocol-version/releases/download/v1.3.0/fix-protocol-version-1.3.0.mirai2.jar -O plugins/fix-protocol-version-1.3.0.mirai2.jar
+wget https://github.com/MrXiaoM/mirai-console-dev-qrlogin/releases/download/0.1.1/mirai-console-dev-qrlogin-0.1.1.jar -O plugins/mirai-console-dev-qrlogin-0.1.1.jar
 
 "../mcl-installer" << EOF
 N
@@ -21,6 +22,8 @@ EOF
 
 chmod +x ./mcl
 
+./mcl --update-package net.mamoe:mirai-core --channel stable --version 2.15.0-M1
+./mcl --update-package net.mamoe:mirai-console --channel stable --version 2.15.0-M1
 ./mcl --update-package net.mamoe:mirai-api-http --channel stable-v2 --type plugin
 ./mcl --update-package xyz.cssxsh.mirai:mirai-device-generator --channel stable --type plugin
 ./mcl --boot-only
